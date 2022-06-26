545b4623406ded148b39fa39e73c44b31be21708
Christopher Dancy <chris.dancy@decentlabs.io>
2022-06-25 20:31:48 -0400

    liquidity hub

--- contracts/LiquidityHub.sol
+++ contracts/LiquidityHub.sol
@@ -6,6 +6,16 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract LiquidityHub {
     using SafeERC20 for IERC20;
 
+    address streamCreator;
+    mapping(address => address) xTokenTodToken;
+    function initalize(address _streamCreator) public {
+        streamCreator = _streamCreator;
+    } 
+
+    function addStream(address xtoken, address dtoken) public {
+        xTokenTodToken[xtoken] = dtoken;
+    }
+
     function cliamAmount(address xtoken, address dtoken) public {
         uint totalXAmount = IERC20(xtoken).balanceOf(address(this));
         uint userDAmount = IERC20(dtoken).balanceOf(msg.sender);
@@ -15,6 +25,8 @@ contract LiquidityHub {
         userXClaim = userDAmount :
         userXClaim = totalXAmount;
 
+        
+        IERC20(dtoken).safeTransferFrom(msg.sender, address(0), (userXClaim * _fairClaim()));
         IERC20(xtoken).safeTransfer(msg.sender, (userXClaim * _fairClaim()));
     }
 
